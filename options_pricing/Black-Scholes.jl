13078070b2da21203e06624dd834603997503e75
Nathaniel Yee <nathany718@gmail.com>
2025-06-14 14:53:07 -0400

    adding heston model

--- options_pricing/Black-Scholes.jl
+++ options_pricing/Black-Scholes.jl
@@ -1,12 +1,13 @@
 # This is a black scholes model written in julia
 using Distributions
-s = 1 #spot
-K = 1 #Strike Price
-r =1 #rf rate
-t = 1 #time to maturity (days)
+s = 100 #spot
+K = 105 #Strike Price
+r =.03 #rf rate
+t = 1/365 #time to maturity (days)
 σ = 1 #volatility
 
 dist = Normal(0,1)
-d1 = log(s/K) + (r+(σ^2/2)) * t
+d1 = (log(s/K) + (r+(σ^2/2)) * t) / (σ * sqrt(t))
 d2 = d1 - (σ * sqrt(t))
-C = (cdf(dist,d1) * s) - (cdf(dist,d2) * (* K * ℯ^(r*t)))
+C = (cdf(dist,d1) * s) - (cdf(dist,d2) * (K * ℯ^(-r*t)))
+print(C)
